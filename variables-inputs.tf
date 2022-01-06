4befde56f6658267a8e711bfc26d93294b6c3f6a
Will Taylor <wrftaylor@gmail.com>
2022-01-06 16:22:32 +0000

    Remove remaining type constraints

--- variables-inputs.tf
+++ variables-inputs.tf
@@ -1,6 +1,4 @@
-variable "env" {
-  type = "string"
-}
+variable "env" {}
 
 # Keyvault
 
@@ -9,6 +7,5 @@ variable "tenant_id" {
 }
 
 variable "jenkins_AAD_objectId" {
-  type        = "string"
   description = "(Required) The Azure AD object ID of a user, service principal or security group in the Azure Active Directory tenant for the vault. The object ID must be unique for the list of access policies."
 }
