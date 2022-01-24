4df146c7e31f8b3032e7167d9caf452cfd9eb8c9
mokainos <72137340+mokainos@users.noreply.github.com>
2022-01-24 16:00:42 +0000

    update

--- db.tf
+++ db.tf
@@ -23,7 +23,7 @@ module "tt-database" {
 //}
 
 data "azurerm_subnet" "postgres" {
-  provider = "azurerm.pet-aks-${var.env}"
+  provider = "azurerm.pet-aks"
   name = "pet_private_${var.env}"
   virtual_network_name = "pet_${var.env}_network"
   resource_group_name  = "pet_${var.env}_network_resource_group"
--- main.tf
+++ main.tf
@@ -16,11 +16,6 @@ resource "azurerm_resource_group" "rg" {
 #PET subnet for privateendpoint
 provider "azurerm" {
   subscription_id = "58a2ce36-4e09-467b-8330-d164aa559c68"
-  alias           = "pet-aks-stg"
-  features {}
-}
-provider "azurerm" {
-  subscription_id = "58a2ce36-4e09-467b-8330-d164aa559c68"
-  alias           = "pet-aks-prod"
+  alias           = "pet-aks"
   features {}
 }
