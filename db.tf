2221d06614ffe2d52e4c182a45fe935dfdb58658
mokainos <72137340+mokainos@users.noreply.github.com>
2022-01-25 11:47:02 +0000

    fmt_and_update

--- db.tf
+++ db.tf
@@ -1,19 +1,19 @@
 module "tt-database" {
-  source                = "git@github.com:hmcts/cnp-module-postgres?ref=pet-disable-dns-setting"
-  product               = var.product
-  component             = ""
-  location              = var.location
-  env                   = var.env
-  postgresql_user       = var.db_postgresql_user
-  database_name         = var.product
-  postgresql_version    = var.db_version
-  common_tags           = var.common_tags
-  subscription          = var.subscription
-  storage_mb            = var.db_storage_mb
-  business_area         = "SDS"
-  key_vault_rg          = "genesis-rg"
-  key_vault_name        = "dtssharedservices${var.env}kv"
-  subnet_id             = data.azurerm_subnet.postgres.id
+  source             = "git@github.com:hmcts/cnp-module-postgres?ref=pet-disable-dns-setting"
+  product            = var.product
+  component          = ""
+  location           = var.location
+  env                = var.env
+  postgresql_user    = var.db_postgresql_user
+  database_name      = var.product
+  postgresql_version = var.db_version
+  common_tags        = var.common_tags
+  subscription       = var.subscription
+  storage_mb         = var.db_storage_mb
+  business_area      = "SDS"
+  key_vault_rg       = "genesis-rg"
+  key_vault_name     = "dtssharedservices${var.env}kv"
+  subnet_id          = data.azurerm_subnet.postgres.id
 }
 
 data "azurerm_subnet" "postgres" {
